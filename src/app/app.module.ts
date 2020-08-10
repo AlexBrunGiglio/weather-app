ae9f9db3e4bb09df8a6fdf35719717f8d41a81fa
Alexandre <alexandre.brungiglio@ynov.com>
2020-08-10 21:00:24 +0200

    debut home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
